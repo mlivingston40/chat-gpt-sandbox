a0b557c24b2c55547e612d3355fb8c246444ce2a
mlivingston40 <matt.j.livingston.40@gmail.com>
2023-05-03 13:46:26 -0400

    random change

--- sampleGPT.py
+++ sampleGPT.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user", "content": "Tell the world about the ChatGPT API in the style of a pirate."}
+            {"role": "user", "content": "30 times 5"}
         ]
     )
 
